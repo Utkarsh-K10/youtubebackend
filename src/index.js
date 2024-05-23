438e80b76a88f8c5d53186eb0fdd0dc667be14f9
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2024-05-23 09:36:56 +0530

    connect to mongo

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+/*
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (error)=>{
+            console.log("Error: ",error)
+            throw error
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is runnig on ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR:", error);
+        throw error;
+    }
+})();
+*/
