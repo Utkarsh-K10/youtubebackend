e522d9c5b69ef6a4255c42c1c03cb12265dc2325
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2024-05-26 16:03:53 +0530

    token generator methods

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -72,6 +72,15 @@ userSchema.methods.generateAccessToken = function () {
         { expiresIn: process.env.ACCESS_TOKEN_EXPIRY })
 }
 
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
+}
+
 
 
 export const User = mongoose.model("User", userSchema);
