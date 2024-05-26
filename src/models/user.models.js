8f3b976788eaed7b35ec5a571ec5ccd3f2efff00
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2024-05-26 15:48:12 +0530

    mongoose password methods

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -55,4 +55,11 @@ userSchema.pre("save", async function(next){
     this.password = bcrypt.hash(this.password, 10)
     next()  
 })
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+
+
 export const User = mongoose.model("User", userSchema);
