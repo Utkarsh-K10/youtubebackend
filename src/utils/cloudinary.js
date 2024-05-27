150c4e6b86ccfd6c8f3f80829cf239f88ed1b918
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2024-05-27 17:59:09 +0530

    cloudinary upload

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,2 +1,29 @@
 import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
 
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const cloudinaryUploader = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            return null
+        }
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        fs.unlinkSync(localFilePath)
+        console.log("Uploaded Successfully at:", response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+};
+
+export {cloudinaryUploader}
