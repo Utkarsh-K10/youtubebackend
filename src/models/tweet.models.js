598ed8020375e44723a5682b5f44d9dada7119c1
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2024-07-06 23:20:45 +0530

    Add tweet model

---
+++ src/models/tweet.models.js
@@ -0,0 +1,24 @@
+import mongoose , { Schema } from "mongoose";
+
+const tweetSchema = new Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    tweet:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    likes:{
+        type: Number,
+        default: 0
+    },
+    comment:{
+        type: String
+    },
+    media:{
+        type: String
+    }
+
+})
